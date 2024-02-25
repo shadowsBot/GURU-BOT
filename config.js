ad2fdd614221170921e0d828e4eab797a87fde20
shadowsBot <151985340+shadowsBot@users.noreply.github.com>
2024-02-25 14:25:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349127031191', 'Darlingron', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['2349127031191'] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
